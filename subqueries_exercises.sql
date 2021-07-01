60cb3c5967e6aec99980a2a6537a1fb2410ccbec
jamesallen0351 <james.allen0351@gmail.com>
2021-07-01 11:17:45 -0500

    updated subqueries exercises no bonus

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -7,7 +7,12 @@ select *
 from employees
 where employees.hire_date = (select hire_date
 from employees
-where employees.emp_no = '101010');
+where employees.emp_no = '101010')
+and emp_no in (
+select emp_no 
+from salaries
+where to_date > now()
+);
 
 
 
@@ -18,28 +23,88 @@ from titles
 where emp_no in (select emp_no
 from employees
 where first_name = 'Aamod') 
-and titles.to_date > curdate()
-group by title;
+and emp_no in (
+select emp_no
+from dept_emp 
+where to_date > now()
+)
+group by title; -- senior staff, staff, engineer, technique leader, senior engineer, assistant engineer
 
 
 -- 3 How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
 
-select *
-from employees;
+SELECT COUNT(*)
+FROM employees
+JOIN dept_emp
+ON dept_emp.emp_no = employees.emp_no
+AND to_date < NOW(); -- 91479 not correct answer
 
-
+select count(*) as 'number of employees no longer employed'
+from employees
+where emp_no not in (
+select emp_no
+from `dept_emp`
+where to_date > now()
+); -- 59900 correct answer
 
 -- 4 Find all the current department managers that are female. List their names in a comment in your code.
 
+select first_name, last_name
+from employees
+join dept_manager using(`emp_no`)
+join departments using(`dept_no`)
+where gender = 'f' and dept_manager.to_date >= now()
+order by dept_name;
 
+
+select *
+from employees
+where emp_no in (
+select emp_no
+from dept_manager
+where to_date > now())
+and gender = 'f';
 
 -- 5 Find all the employees who currently have a higher salary than the companies overall, historical average salary.
+select *
+from employees
+where emp_no in(
+select emp_no
+from salaries
+where salary > (select avg(salary) from salaries)
+and to_date > now()
+); -- 154543
+
+select *
+from employees
+where emp_no in(
+select emp_no
+from salaries
+where salary > (select avg(salary) from salaries)
+)
+and emp_no in (
+select emp_no
+from dept_emp
+where to_date > now()); -- 154726
 
 
 
--- 6 How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
+-- 6 How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?  -- 83
 
+select count(*)
+from salaries
+where to_date > now() 
+and salary > (
+select max(salary) - std(salary)
+from salaries
+where to_date > now()
+); -- 83
 
+select (select count(*)
+from salaries
+where to_date > now());  -- 240124
+
+select (83/240124)*100;
 
 
 # Bonus questions
